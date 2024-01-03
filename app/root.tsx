b426f4ddab01a5da022d4d6746097636177f601b
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-03 12:47:10 +0545

    complete contact

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,42 @@
+import { json, redirect } from "@remix-run/node";
 import {
   Form,
+    Link,
   Links,
   LiveReload,
   Meta,
+    NavLink,
+    Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+    useNavigation,
 } from "@remix-run/react";
 
+import { createEmptyContact, getContacts } from "./data";
+
+import type {LinksFunction,LoaderFunctionArgs,} from "@remix-run/node";
+import appStylesHref from "./app.css"
+
+export const action = async () => {
+    const contact = await createEmptyContact();
+    return redirect(`/contacts/${contact.id}/edit`);
+};
+export const loader = async ({request,}: LoaderFunctionArgs) => {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    return json({ contacts });
+};
+export const links: LinksFunction = () => {
+  return [{rel: "stylesheet", href: appStylesHref}];
+}
+
 export default function App() {
-  return (
+  const { contacts } = useLoaderData<typeof loader>();
+    const navigation = useNavigation();
+
+    return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
@@ -17,38 +45,71 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
+      <div id="sidebar">
+        <h1>Remix Contacts</h1>
+        <div>
+          <Form id="search-form" role="search">
+            <input
                 id="q"
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
+            />
+            <div id="search-spinner" aria-hidden hidden={true}/>
+          </Form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
         </div>
+        <nav>
+          {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                    <NavLink
+                        className={({isActive, isPending}) =>
+                            isActive
+                                ? "active"
+                                : isPending
+                                    ? "pending"
+                                    : ""
+                        }
+                        to={`contacts/${contact.id}`}
+                    >
+                        <li key={contact.id}>
+                            <Link to={`contacts/${contact.id}`}>
+                                {contact.first || contact.last ? (
+                                    <>
+                                        {contact.first} {contact.last}
+                                    </>
+                                ) : (
+                                    <i>No Name</i>
+                                )}{" "}
+                                {contact.favorite ? (
+                                    <span>★</span>
+                                ) : null}
+                            </Link>
+                        </li>
+                    </NavLink>
 
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+                ))}
+              </ul>
+          ) : (
+              <p>
+                  <i>No contacts</i>
+              </p>
+          )}
+        </nav>
+      </div>
+      <div className={
+        navigation.state === "loading" ? "loading" : ""
+      } id="detail">
+          <Outlet/>
+      </div>
+
+      <ScrollRestoration/>
+      <Scripts/>
+      <LiveReload/>
       </body>
     </html>
   );
---
+++ app/routes/_index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+    return (
+        <p id="index-page">
+            <br />
+
+        </p>
+    );
+}
